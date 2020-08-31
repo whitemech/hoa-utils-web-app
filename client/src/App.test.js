7de653d35b68966ad3aed5e525d843987ff6427b
MarcoFavorito <marco.favorito@gmail.com>
2020-08-31 10:16:47 +0200

    update client tests

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import App from './App';
+import { shallow, mount } from 'enzyme';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it('renders without crashing', () => {
+  shallow(<App />);
 });
+
+// it('full rendering without crashing', () => {
+//   mount(<App />);
+// });
